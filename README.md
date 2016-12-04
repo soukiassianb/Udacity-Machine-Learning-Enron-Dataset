4e226df027e2dca4efc41e819dd83ff52e9e7e31
soukiab <soukiassianb@gmail.com>
2016-12-04 13:06:23 +0100

    Improvements

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
 # Identify Fraud from Enron email
 
-https://review.udacity.com/#!/rubrics/27/view
-https://docs.google.com/document/d/17-JwNQH1aRxtqMkJ6zpCL_68kh5F6uSbDXcJS26vZWY/pub
-
-
 ## Enron Submission Free-Response Questions
 
 **1. Summarize for us the goal of this project and how machine learning is useful in trying to accomplish it. As part of your answer, give some background on the dataset and how it can be used to answer the project question. Were there any outliers in the data when you got it, and how did you handle those?  [relevant rubric items: “data exploration”, “outlier investigation”]**
@@ -17,45 +13,49 @@ Machine Learning is a good tool for such a project for at least two reasons:
 - The Enron email corpus is a huge dataset: trying to process data and recognize patterns using regular techniques would be long and inefficient.
 - We don't really know what we are looking for, should we suspect people with huge bonuses ? A lot of shared receipts with POI ? It's hard to know and this is the kind of problem at which ML is good.
 
-While preparing the dataset for the project I was able to identify a few outliers using data visualization. I also decided to use a script to remove other extreme values from the data (top and below 2% percentiles).
+The dataset we'll use is composed of 146 executives, 21 of which are persons of interest. This is quite unbalanced and we'll need to take this into account when we evaluate our model.
 
+The dataset has a total of 3066 data points.
+While preparing the dataset for the project I was able to identify a few outliers:
+- "TOTAL" which may refer to the company, not an individual, had bonus and salary orders of magnitude higher than the average values for these variables.
+- "LOCKHART EUGENE E" had only "NaN" values.
+- "THE TRAVEL AGENCY IN THE PARK" also nearly only had "NaN" values.
+
+I also used Pandas to compute values of total payments and stock value from the dataset and compared with the values on the FindLaw pdf file. When finding non-matching values, I updated the dataset with the values from the file.
 
 **2. What features did you end up using in your POI identifier, and what selection process did you use to pick them? Did you have to do any scaling? Why or why not? As part of the assignment, you should attempt to engineer your own feature that does not come ready-made in the dataset -- explain what feature you tried to make, and the rationale behind it. (You do not necessarily have to use it in the final analysis, only engineer and test it.) In your feature selection step, if you used an algorithm like a decision tree, please also give the feature importances of the features that you use, and if you used an automated feature selection function like SelectKBest, please report the feature scores and reasons for your choice of parameter values.  [relevant rubric items: “create new features”, “properly scale features”, “intelligently select feature”]**
 
-In my POI identify I ended up using a mix of financial and email features:
-- salary (Financial)
-- bonus (Financial)
-- deferral_payments (Financial)
-- restricted_stock_deferred (Financial)
-- long_term_incentive (Financial)
-- from_this_person_to_poi (Email)
-- shared_receipt_with_poi (Email)
+In my POI identifier I ended up using a mix of financial and email features.
+I imported most of the features available and created two new ones: from_poi_ratio and to_poi_ratio. They are respectively the fraction of messages received from POI among all messages received and the fraction of messages send to POI among all messages send. Including the fractions rather than the absolutes values should create more useful features because the data points are normalized.
 
-I ran a simple decision tree and checked the score after adding and removing different features.
+I preprocessed all the features using StandardScaler and then used SelectKbest in combinaison with PCA within a GridSearchCV to get the best features for the final pipeline.
 
-I tried feature scaling like standardization using scikit learn StandardScaler but did not gain precision, I however did gain a little precision using PCA with 4 components, which I decided to use for the final classifier.
+Getting later on the features scores from SelectKBest, it appears that to_poi_ratio, (score: 8.43), is the fourth most important feature after 'from_poi_to_this_person', 'Bonus' and 'deferred_income'. The GridSearch feature selection process selected the two main components from the PCA. 
 
-I created two features from_poi_ratio and to_poi_ratio which are respectively the fraction of messages received from POI among all messages received and the fraction of messages send to POI among all messages send.
+When running the exact same model but removing the two custom features I go from ~0.34 precision, ~0.32 recall to ~0.20 precision, ~0.19 recall.
 
 **3. What algorithm did you end up using? What other one(s) did you try? How did model performance differ between algorithms?  [relevant rubric item: “pick an algorithm”]
 What does it mean to tune the parameters of an algorithm, and what can happen if you don’t do this well?  How did you tune the parameters of your particular algorithm? (Some algorithms do not have parameters that you need to tune -- if this is the case for the one you picked, identify and briefly explain how you would have done it for the model that was not your final choice or a different model that does utilize parameter tuning, e.g. a decision tree classifier).  [relevant rubric item: “tune the algorithm”]
 What is validation, and what’s a classic mistake you can make if you do it wrong? How did you validate your analysis?  [relevant rubric item: “validation strategy”]**
 
-I ended up using a RandomForestClassifier. I picked this one after trying many other classifiers including Adaboost, SVM, KNN with different parameters. The average performance of RandomForest seemed nearly always better on precision, not on recall.
+I ended up using a K-Nearest Neighbors Classifier after trying Adaboost, Gradient Boosting Classifier, SVC, Decision Tree, RandomForest and others.
 
-To choose which parameters to use in the classifier I used GridSearchCV with recall set as score parameter to improve the recall score.
+It was the only one which gave me consistently statisfying results on both precision and recall. Other algorithms like RandomForest were much better on precision but I wasn't able to get them to perform well on recall. With other algorithms like SVC I was able to have a near perfect recall but low precision.
 
-I got and used as a result:
+For each algorithm I tried different combinaisons of parameters within grid_search and alternated between f1 and recall as scoring parameters. 
 
-- 20 trees
-- entropy as the information gain function
-- no bootstrap
-- 2 min_samples_split
-- 4 max_features
+We choose parameters to adapt the Algorithm to the problem at hand. Data comes in all sorts and forms and so no single Algorithm can perform well for all our needs.
+When choosing the parameters, the first risk is to not tune the algorithm well enough and to have low performance.
+The second risk is to overfit the training data. When this happens the model is unlikely to generalize well onto unseen data and so has a lower performance.
 
+The answer to those problems is validation. The goal of validation is to give an estimate of the performance of our model on a independent dataset and to check and prevent overfitting. The way this works is that we split the data between training and testing sets, and we train the model with the training set and test with the testing set, that is unseen data.
+
+In our case, we use StratifiedShuffleSplit to split the data into randomized folds (subsets of our datasets) that preserve the percentage of samples for each class.
+We fit our grid search and use the resulting best parameters within the pipeline.
+We use the pipeline as the final model. The scoring validation was made using a classification_report with our test data and predictions from test features. as well as using the final `tester.py` file.
 
 **4. Give at least 2 evaluation metrics and your average performance for each of them.  Explain an interpretation of your metrics that says something human-understandable about your algorithm’s performance. [relevant rubric item: “usage of evaluation metrics”]**
 
 I chose precision and recall as reference metrics.
-Average precision: 0.42, which means 42% of people who are predicted as POI are really POI.
-Average recall: 0.22, means that among the POI, 22% are identified by our classifier.
+Average precision: 0.348, which means 34.8% of people who are predicted as POI are really POI.
+Average recall: 0.326, means that among the POI, 32.6% are identified by our classifier.
--- poi_id.py
+++ poi_id.py
@@ -1,56 +1,112 @@
+# coding: utf-8
 #!/usr/bin/python
 from __future__ import division
 import os
 import sys
-os.chdir(os.path.dirname(os.path.realpath(__file__)))
 sys.path.append("../tools/")
 import pickle
 from feature_format import featureFormat, targetFeatureSplit
 from tester import dump_classifier_and_data
-from sklearn.cross_validation import train_test_split
+from sklearn.cross_validation import train_test_split, StratifiedShuffleSplit
 import matplotlib.pyplot as plt
 from operator import itemgetter
 import numpy as np
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
 from sklearn.grid_search import GridSearchCV
 from sklearn.decomposition import PCA
+from sklearn.pipeline import Pipeline
+from sklearn.feature_selection import SelectKBest, f_classif
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import FeatureUnion
+from sklearn.metrics import classification_report
+import pandas as pd
 
 # features selected
 features_list = ['poi',
-                 'salary',
                  'bonus',
+                 'salary',
                  'deferral_payments',
-                 'restricted_stock_deferred',
-                 'long_term_incentive',
+                 'deferred_income',
+                 'director_fees',
+                 'exercised_stock_options',
+                 'expenses',
+                 'from_messages',
+                 'from_poi_to_this_person',
                  'from_this_person_to_poi',
-                 'shared_receipt_with_poi']
+                 'loan_advances',
+                 'long_term_incentive',
+                 'other',
+                 'restricted_stock',
+                 'restricted_stock_deferred',
+                 'shared_receipt_with_poi',
+                 'to_messages',
+                 'total_payments',
+                 'total_stock_value']
 
 # Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
 
 
+# In[314]:
+
 # Remove outliers
 data_dict.pop('TOTAL', 0)
 data_dict.pop('LOCKHART EUGENE E', 0)
 data_dict.pop('THE TRAVEL AGENCY IN THE PARK', 0)
 
+# We load the dataset into a dataframe
+# and compute total for payments and stock values
+# We'll then compare the results with the provided financial pdf file.
 
-def get_array(data_dict, value):
-    return np.array([int(v[value]) for v in data_dict.values() if v[value] != 'NaN'])
+df = pd.DataFrame.from_dict(data_dict).T
+df.replace('NaN', np.nan, inplace=True)
 
+payment_total_fields = ['salary',
+                        'bonus',
+                        'long_term_incentive',
+                        'deferred_income',
+                        'deferral_payments',
+                        'loan_advances',
+                        'other',
+                        'expenses',
+                        'director_fees']
 
-def clean_extreme_values(data_dict, feature, percentile):
-    """Remove values above and below given percentile"""
-    high_threshold = np.percentile(get_array(data_dict, feature), 100 - percentile)
-    low_threshold = np.percentile(get_array(data_dict, feature), percentile)
-    for k, v in data_dict.items():
-        if v[feature] > high_threshold or v[feature] < low_threshold:
-            data_dict[k][feature] = 'NaN'
-    return data_dict
+total_stock_value_fields = ['exercised_stock_options',
+                            'restricted_stock',
+                            'restricted_stock_deferred']
 
-for feature in features_list[1:]:
-    data_dict = clean_extreme_values(data_dict, feature, 2)
+with pd.option_context('display.max_rows', 999, 'display.max_columns', 3):
+    # Set to true to print the results
+    if False:
+        print(df[payment_total_fields].sum(axis=1))
+    if False:
+        print(df[total_stock_value_fields].sum(axis=1))
+
+# After printing the results and comparing with the file
+# BELFER ROBERT and BHATNAGAR SANJAY appeared to have incorrect total values
+# so I updated the values within the dataset using the ones on the pdf file.
+data_dict['BELFER ROBERT']['deferred_income'] = -102500
+data_dict['BELFER ROBERT']['deferral_payments'] = 0
+data_dict['BELFER ROBERT']['expenses'] = 3285
+data_dict['BELFER ROBERT']['director_fees'] = 102500
+data_dict['BELFER ROBERT']['total_payments'] = 3285
+data_dict['BELFER ROBERT']['exercised_stock_options'] = 0
+data_dict['BELFER ROBERT']['restricted_stock'] = 44093
+data_dict['BELFER ROBERT']['restricted_stock_deferred'] = -44093
+data_dict['BELFER ROBERT']['total_stock_value'] = 0
+
+data_dict['BHATNAGAR SANJAY']['total_payments'] = 137864
+data_dict['BHATNAGAR SANJAY']['expenses'] = 137864
+data_dict['BHATNAGAR SANJAY']['other'] = 0
+data_dict['BHATNAGAR SANJAY']['director_fees'] = 0
+data_dict['BHATNAGAR SANJAY']['exercised_stock_options'] = 15456290
+data_dict['BHATNAGAR SANJAY']['restricted_stock'] = 2604490
+data_dict['BHATNAGAR SANJAY']['restricted_stock_deferred'] = -2604490
+data_dict['BHATNAGAR SANJAY']['total_stock_value'] = 15456290
 
 
 # Create new features
@@ -77,46 +133,91 @@ my_dataset = data_dict
 data = featureFormat(my_dataset, features_list, sort_keys=True)
 labels, features = targetFeatureSplit(data)
 
-
-# PCA
-pca = PCA(n_components=4)
-pca.fit(features)
-features = pca.transform(features)
-
-features_train, features_test, labels_train, labels_test = \
-    train_test_split(features, labels, test_size=0.3, random_state=42)
-
-
-def findRandomForestParameters():
-    """
-    Use grid_search to determine parameters for our RandomForestClassifier
-    """
-    clf = RandomForestClassifier(random_state=42)
-    params = {
-        "n_estimators": range(20, 60),
-        "criterion": ["gini", "entropy"],
-        "max_features": range(3, 5),
-        "min_samples_split": range(2, 4),
-        "bootstrap": [True, False]
-    }
-    grid_search = GridSearchCV(clf, params, n_jobs=-1, cv=2, scoring='recall')
-    grid_search.fit(features_train, labels_train)
-    print grid_search.best_params_
-
-# findRandomForestParameters()
+# We use StratifiedShuffleSplit because the dataset is small and unbalanced
+cv = StratifiedShuffleSplit(labels, n_iter=100, test_size=0.75, random_state=42)
+for train_index, test_index in cv:
+    X_train = []
+    X_test = []
+    y_train = []
+    y_test = []
+    for ii in train_index:
+        X_train.append(features[ii])
+        y_train.append(labels[ii])
+    for jj in test_index:
+        X_test.append(features[jj])
+        y_test.append(labels[jj])
 
 
-# Load our classifier with parameters
-# determiend by findRandomForestParameters
-clf = RandomForestClassifier(max_features=4,
-                             min_samples_split=2,
-                             bootstrap=False,
-                             criterion='entropy',
-                             n_estimators=20)
+# In[319]:
 
 
-# fit our classifier
-clf.fit(features_train, labels_train)
+feature_selection = FeatureUnion([
+        ('kbest', SelectKBest(f_classif)),
+        ('pca', PCA())
+    ])
 
-# dump classifier and data
+pipeline = Pipeline([
+        ('scaler', StandardScaler()),
+        ('feature_selection', feature_selection),
+        # ('clf', RandomForestClassifier())
+        # ('clf', AdaBoostClassifier())
+        # ('clf', GradientBoostingClassifier())
+        # ('clf', SVC())
+        # ('clf', DecisionTreeClassifier())
+        ('clf', KNeighborsClassifier(weights='distance', algorithm='ball_tree'))
+    ])
+
+grid_search = GridSearchCV(pipeline, {
+        'feature_selection__kbest__k': [2, 3, 4, 5, 7, 10],
+        'feature_selection__pca__n_components': [2, 5, 10, ],
+
+        # ADABOOST
+        # 'clf__algorithm' : ['SAMME', 'SAMME.R'],
+        # 'clf__n_estimators': [25, 50, 100],
+        # 'clf__learning_rate': [.5, 1., 1.5],
+
+        # GRADIENT BOOSTING
+        # 'clf__loss' : ['deviance', 'exponential'],
+        # 'clf__learning_rate': [0.1, 0.3, 0.5],
+        # 'clf__n_estimators': [25, 50, 100],
+        # 'clf__max_depth': [3, 5, 10],
+        # 'clf__min_samples_split': [1, 3, 5, 10],
+
+        # SVM
+        # 'clf__kernel': [ 'sigmoid', 'poly','rbf'],
+        # 'clf__C': [1, 5, 10, 20, 200, 1000],
+        # 'clf__class_weight' :[None, 'balanced'],
+
+        # RANDOM FOREST
+        # 'clf__n_estimators': [25, 30, 50, 80, 100],
+        # 'clf__min_samples_split': [1, 3, 5, 10],
+        # 'clf__criterion': ['gini', 'entropy'],
+        # 'clf__max_depth': [3, 6, 8, 11, 15, 20]
+
+        # KNC
+        'clf__n_neighbors': [2, 4, 6, 10],
+        'clf__weights': ['distance', 'uniform'],
+        'clf__algorithm': ['kd_tree', 'ball_tree', 'auto', 'brute'],
+
+    }, scoring='recall')
+
+
+grid_search.fit(X_train, y_train)
+
+clf = pipeline.set_params(**grid_search.best_params_)
+pipeline.fit(X_train, y_train)
+
+print(grid_search.best_params_)
+print(dir(grid_search))
+
+report = classification_report(y_test, clf.predict(X_test))
+print report
+
+# dump classifier and dta
 dump_classifier_and_data(clf, my_dataset, features_list)
+
+# Getting the feature Scores
+k = grid_search.get_params(True)['estimator__feature_selection__transformer_list'][0][1]
+features_scores = zip(features_list[1:], k.scores_)
+for f, s in sorted(features_scores, key=lambda x: x[1], reverse=True):
+    print('%s: %s' % (f, s))
